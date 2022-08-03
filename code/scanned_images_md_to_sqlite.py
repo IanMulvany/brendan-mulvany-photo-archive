febdd0d5984ffdbcbf487fd7dbc4298820d0180d
Ian Mulvany <ian@mulvany.net>
2022-08-03 19:11:36 +0100

    tidy up some whitespace

--- code/scanned_images_md_to_sqlite.py
+++ code/scanned_images_md_to_sqlite.py
@@ -7,7 +7,6 @@ We assume that there is one directory per scanning event, and each scan is for o
 batch of images from the Brendan Mulvany archive. 
 """
 
-# Get some config data about the location of the myslite db
 import configparser
 import argparse
 
@@ -15,7 +14,6 @@ import argparse
 config = configparser.ConfigParser()
 config.read("config.ini")
 db_path = config["sqlite3"]["db_path"]
-
 print(db_path)
 
 
