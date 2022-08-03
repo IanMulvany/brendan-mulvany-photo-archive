6305a7e5deae7dd3926f25514430fd2651939502
Ian Mulvany <ian@mulvany.net>
2022-08-03 11:21:22 +0100

    modified the DB creation script to create more fields and a descriptoin table

--- code/create_image_sqlite_db.py
+++ code/create_image_sqlite_db.py
@@ -15,8 +15,13 @@ def create_image_db(db_path):
     conn = sqlite3.connect(db_path)
     cur = conn.cursor()
     cur.execute("DROP TABLE IF EXISTS Images")
+    cur.execute("DROP TABLE IF EXISTS Images2")
+    cur.execute("DROP TABLE IF EXISTS FieldDescription")
     cur.execute(
-        "CREATE TABLE Images (image_id INTEGER PRIMARY KEY, capture_date TEXT, image_path TEXT, image_name TEXT, image_size INTEGER, image_hash TEXT)"
+        "CREATE TABLE Images (image_id INTEGER PRIMARY KEY, capture_date TEXT, image_path TEXT, image_name TEXT, image_size INTEGER, image_hash TEXT, near_hash TEXT, bm_batch_year TEXT, bm_batch_number INTEGER, bm_batch_note TEXT) "
+    )
+    cur.execute(
+        "CREATE TABLE FieldDescription (image_id_description TEXT, capture_date_description TEXT, image_path_description TEXT, image_name_description TEXT, image_size_description TEXT, image_hash_description TEXT, near_hash_description TEXT, bm_batch_description TEXT, bm_batch_number_description INTEGER, bm_batch_note_description TEXT) "
     )
     conn.commit()
     conn.close()
