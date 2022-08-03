5a7bf83364b40dde64f0aa8304b6e69a200519b4
Ian Mulvany <ian@mulvany.net>
2022-08-03 19:11:07 +0100

    added argparse to get path to image directory

--- code/scanned_images_md_to_sqlite.py
+++ code/scanned_images_md_to_sqlite.py
@@ -17,3 +17,17 @@ config.read("config.ini")
 db_path = config["sqlite3"]["db_path"]
 
 print(db_path)
+
+
+# use argparse to get the path to the image files
+parser = argparse.ArgumentParser(
+    prog="scanned_images_md_to_sqlite",
+    description="Get some metadata about the images in a directory",
+    epilog="good scanning!!",
+)
+parser.add_argument(
+    "image_dir_path", type=str, help="path to the directory of scanned images"
+)
+args = parser.parse_args()
+image_dir_path = args.image_dir_path
+print("images you are looking at are in the directory: " + image_dir_path)
